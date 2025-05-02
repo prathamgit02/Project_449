0e9079206bf1855268e2126bc0dd6a6bbaa36b1c
prathamgit02 <patelpratham1208@csu.fullerton.edu>
2025-05-01 22:52:43 -0700

    booking commit

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,11 +1,10 @@
-import { fetchPropertyDetails } from '@/utils/actions';
+import { fetchPropertyDetails, findExistingReview } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 import BreadCrumbs from '@/components/properties/BreadCrumbs';
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 import ShareButton from '@/components/properties/ShareButton';
 import ImageContainer from '@/components/properties/ImageContainer';
 import PropertyRating from '@/components/card/PropertyRating';
-import BookingCalendar from '@/components/properties/BookingCalendar';
 import PropertyDetails from '@/components/properties/PropertyDetails';
 import UserInfo from '@/components/properties/UserInfo';
 import Description from '@/components/properties/Description';
@@ -13,12 +12,23 @@ import { Separator } from '@/components/ui/separator';
 import Amenities from '@/components/properties/Amenities';
 import dynamic from 'next/dynamic';
 import { Skeleton } from '@/components/ui/skeleton';
+import SubmitReview from '@/components/reviews/SubmitReview';
+import PropertyReviews from '@/components/reviews/PropertyReviews';
+import { auth } from '@clerk/nextjs/server';
 
 const DynamicMap = dynamic(
   () => import('@/components/properties/PropertyMap'),
   {
     //ssr: false,
     loading: () => <Skeleton className='h-[400px] w-full' />,
+  }
+);
+
+const DynamicBookingWrapper = dynamic(
+  () => import('@/components/booking/BookingWrapper'),
+  {
+    //ssr: false,
+    loading: () => <Skeleton className='h-[200px] w-full' />,
   }
 );
 
@@ -29,6 +39,14 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const details = { baths, bedrooms, beds, guests };
   const firstName = property.profile.firstName;
 const profileImage = property.profile.profileImage;
+
+const { userId } = auth();
+  const isNotOwner = property.profile.clerkId !== userId;
+  const reviewDoesNotExist =
+    userId && isNotOwner && !(await findExistingReview(userId, property.id));
+
+
+
   return (
     <section>
       <BreadCrumbs name={property.name} />
@@ -51,14 +69,22 @@ const profileImage = property.profile.profileImage;
       <Separator className='mt-4' />
 <Description description={property.description} />
 <Amenities amenities={property.amenities} />
-<DynamicMap countryCode={property.country} />;
+<DynamicMap countryCode={property.country} />
 
       </div>
     <div className='lg:col-span-4 flex flex-col items-center'>
       {/* calendar */}
-      <BookingCalendar />
+      <DynamicBookingWrapper
+      propertyId={property.id}
+      price={property.price}
+      bookings={property.bookings}
+    />
     </div>
   </section>
+
+  {reviewDoesNotExist && <SubmitReview propertyId={property.id} />};
+  <PropertyReviews propertyId={property.id} />
+
     </section>
   );}
 export default PropertyDetailsPage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,11 +1,13 @@
 'use server';
 
-import { imageSchema, profileSchema, propertySchema, validateWithZodSchema } from './schemas';
+import { createReviewSchema, imageSchema, profileSchema, propertySchema, validateWithZodSchema } from './schemas';
 import db from './db';
 import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { uploadImage } from './ supabase';
+import Rating from '@/components/reviews/Rating';
+import { count } from 'console';
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -263,6 +265,133 @@ export const fetchPropertyDetails = async (id: string) => {
     },
     include: {
       profile: true,
+      bookings: {
+        select: {
+          checkIn: true,
+          checkOut: true,
+        },
+      },
     },
   });
+};
+
+export async function createReviewAction(prevState: any, formData: FormData) {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+
+    const validatedFields = validateWithZodSchema(createReviewSchema, rawData);
+    await db.review.create({
+      data: {
+        ...validatedFields,
+        profileId: user.id,
+      },
+    });
+    revalidatePath(`/properties/${validatedFields.propertyId}`);
+    return { message: 'Review submitted successfully' };
+  } catch (error) {
+    return renderError(error);
+  }
+}
+
+export async function fetchPropertyReviews(propertyId: string) {
+  const reviews = await db.review.findMany({
+    where: {
+      propertyId,
+    },
+    select: {
+      id: true,
+      rating: true,
+      comment: true,
+      profile: {
+        select: {
+          firstName: true,
+          profileImage: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+  return reviews;
+}
+
+export const fetchPropertyReviewsByUser = async () => {
+  const user = await getAuthUser();
+  const reviews = await db.review.findMany({
+    where: {
+      profileId: user.id,
+    },
+    select: {
+      id: true,
+      rating: true,
+      comment: true,
+      property: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
+  return reviews;
+};
+
+export const deleteReviewAction = async (prevState: { reviewId: string }) => {
+  const { reviewId } = prevState;
+  const user = await getAuthUser();
+
+  try {
+    await db.review.delete({
+      where: {
+        id: reviewId,
+        profileId: user.id,
+      },
+    });
+
+    revalidatePath('/reviews');
+    return { message: 'Review deleted successfully' };
+  } catch (error) {
+    return renderError(error);
+  }
+};
+
+
+export async function fetchPropertyRating(propertyId:string){
+  const result = await db.review.groupBy({
+    by:['propertyId'],
+    _avg:{
+      rating:true
+    },
+    _count:{
+      rating:true
+    },
+    where:{
+      propertyId,
+    },
+  });
+  return {
+    rating: result[0]?._avg.rating?.toFixed() ?? 0,
+    count: result[0]?._count.rating ?? 0,
+  }
+}
+
+
+
+export const findExistingReview = async (
+  userId: string,
+  propertyId: string
+) => {
+  return db.review.findFirst({
+    where: {
+      profileId: userId,
+      propertyId: propertyId,
+    },
+  });
+};
+
+export const createBookingAction = async (prevState:{propertyId:string, checkIn:Date, checkOut:Date
+}) => {
+  return { message: 'create booking' };
 };
